f553c22351d0e569dd77c1bc47cb58ef51eb2912
Titoff Roma <64364502+Titoffroma@users.noreply.github.com>
2020-11-08 15:52:22 +0200

    add dinamic lenth to switchColors

--- src/index.js
+++ src/index.js
@@ -3,28 +3,27 @@ import colors from './js/colors';
 
 const startBtn = document.querySelector("[data-action='start']");
 const endBtn = document.querySelector("[data-action='stop']");
-const body = document.querySelector("body");
+const body = document.querySelector('body');
 
 let switchSet;
 
 const randomIntegerFromInterval = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  };
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
 
 const switchMe = () => {
-    body.style.background = colors[randomIntegerFromInterval(0, 5)];
-}
+  body.style.background =
+    colors[randomIntegerFromInterval(0, colors.length - 1)];
+};
 
 const switchColors = () => {
-    endBtn.addEventListener('click', end, {once: true});
-    switchSet = setInterval(switchMe, 1000);
-}
+  endBtn.addEventListener('click', end, { once: true });
+  switchSet = setInterval(switchMe, 1000);
+};
 
 const end = () => {
-    clearInterval(switchSet);
-    startBtn.addEventListener('click', switchColors, {once: true});
-}
+  clearInterval(switchSet);
+  startBtn.addEventListener('click', switchColors, { once: true });
+};
 
-startBtn.addEventListener('click', switchColors, {once: true});
-
-
+startBtn.addEventListener('click', switchColors, { once: true });
